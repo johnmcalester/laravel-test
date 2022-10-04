0dc77faf07d13c2149b088067d12a6bb4d1532dd
John McAlester <john@thriveglobal.com>
2022-10-04 14:41:49 -0700

    Add listings migration and seeding data

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,33 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Aliquam vitae voluptatibus, placeat a molestiae qui rem in accusamus illum sunt labore nulla ut tenetur. 
-                Suscipit saepe aperiam unde ad at.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Aliquam vitae voluptatibus, placeat a molestiae qui rem in accusamus illum sunt labore nulla ut tenetur. 
-                Suscipit saepe aperiam unde ad at.'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,5 +1,8 @@
+<p>
+    <a href="http://localhost/"><- Back</a>
+</p>
 <h2>
-    <a href="/listing/{{$listing['id']}}">{{$listing['title']}}</a>
+    {{$listing['title']}}
 </h2>
 <p>
     {{$listing['description']}}
