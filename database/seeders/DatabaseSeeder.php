6257875d994c6334115270ada867047bcaba5b4a
John McAlester <john@thriveglobal.com>
2022-10-08 11:26:46 -0700

    Add user id field and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -15,13 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-
-        Listing::factory(6)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // \App\Models\User::factory(10)->create();
+        
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@email.com'
+        ]);
+        
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
